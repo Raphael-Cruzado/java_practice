b52974e79fdcd8b13b2c47fb47be8d04d9889f24
Lance Cruzado <raphaellcruzado@gmail.com>
2023-11-23 03:04:53 -0800

    Adding sumFileContents.java

--- src/SumFileContents/sumFileContents.java
+++ src/SumFileContents/sumFileContents.java
@@ -1,18 +1,17 @@
-package AaPractice;
+package SumFileContents;
 
-import java.io.FileInputStream;
-import java.util.Scanner;
-import java.io.IOException;
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class practice {
+public class sumFileContents {
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        String parentPath = "C:\\Users\\rapha\\IdeaProjects\\java_practice\\src\\AaPractice\\";
+        String parentPath = "C:\\Users\\rapha\\IdeaProjects\\java_practice\\src\\SumFileContents\\";
         String fileName = parentPath + getFileName();
 
-        int sum = sumFileContents(fileName);
+        int sum = sumContents(fileName);
 
         System.out.println("The sum is: " + sum);
     }
@@ -22,7 +21,7 @@ public class practice {
         return input.nextLine();
     }
 
-    public static int sumFileContents(String fileName) throws IOException {
+    public static int sumContents(String fileName) throws IOException {
         int sum = 0;
 
         try (Scanner fileScanner = new Scanner(new File(fileName))) {
