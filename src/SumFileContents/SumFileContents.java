41cd7359465b643784e5369c798617fef1fd3b3a
Lance Cruzado <raphaellcruzado@gmail.com>
2023-11-23 03:07:00 -0800

    Adding sumFileContents.java

--- src/SumFileContents/SumFileContents.java
+++ src/SumFileContents/SumFileContents.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class sumFileContents {
+public class SumFileContents {
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
